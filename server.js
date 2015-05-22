4fe2cfaebb9d367504594168f35087711b5b26da
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-05-21 22:08:51 -0600

    Added management for custom inputs

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var server = require('websocket').server, http = require('http');
 
+var myArr = [];
+
+
 var socket = new server({
     httpServer: http.createServer().listen(8066)
 });
@@ -8,11 +11,21 @@ socket.on('request', function(request) {
     var connection = request.accept(null, request.origin);
 
     connection.on('message', function(message) {
-        console.log(message.utf8Data);
-        connection.sendUTF('hello');
-        setTimeout(function() {
-            connection.sendUTF('this is a websocket example');
-        }, 1000);
+      console.log(message.utf8Data);
+
+      var messageString = '';
+      
+      if(message.utf8Data !== '@reload') {
+        // add message to application array
+        myArr.push(message.utf8Data);
+      }
+
+      myArr.forEach( function( content ) {
+        messageString += content + '<br />';
+      });
+
+
+      connection.sendUTF( messageString );
     });
 
     connection.on('close', function(connection) {
