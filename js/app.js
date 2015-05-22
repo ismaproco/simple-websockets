07a18e34d1e119a41fb1829d11da602c7a4dfd6d
Ismael Jimenez <ijimenezgarzon@gmail.com>
2015-05-21 22:09:15 -0600

    Added timing events to show the result

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 // Application code here
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  documentLoad();
+});
+
+
+function documentLoad() {
+    var button = document.getElementById('sendButton');
+    var message = document.getElementById('messageText');
+
+    button.addEventListener('click', function(event) {
+      socket.send( message.value );
+    });
+
+    setInterval(reloadMessageListing, 1000);
+}
+
+
+function reloadMessageListing() {
+  socket.send( '@reload' );
+}
